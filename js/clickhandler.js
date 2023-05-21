aed4fbb163104f542b6a1d3b4bf00d089be95f0d
Илья Захаров <zakharovellia@Aska.local>
2023-05-21 15:45:32 +0300

    3

--- js/clickhandler.js
+++ js/clickhandler.js
@@ -2,6 +2,11 @@ const button = document.querySelector(".btn");
 
 function handleBtn() {
   console.log("Yo! It's me, JS!");
+  let url = "http://85.192.48.111:8080";
+
+  fetch(url)
+    .then((response) => response.json()) // Декодируем ответ в формате json
+    .then((data) => console.log(data)); // Выводим ответ в консоль
 }
 
 button.addEventListener("click", handleBtn);
