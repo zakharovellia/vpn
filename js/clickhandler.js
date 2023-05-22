691b91b9b6f7f29316cd0eeb00c368b03d248ba8
Илья Захаров <zakharovellia@Aska.local>
2023-05-22 20:48:33 +0300

    4

--- js/clickhandler.js
+++ js/clickhandler.js
@@ -2,7 +2,7 @@ const button = document.querySelector(".btn");
 
 function handleBtn() {
   console.log("Yo! It's me, JS!");
-  let url = "http://85.192.48.111:8080";
+  let url = "http://0.0.0.0:8080";
 
   fetch(url)
     .then((response) => response.json()) // Декодируем ответ в формате json
--- python/env/bin/activate.fish
+++ python/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/zakharovellia/code/python/env"
+set -gx VIRTUAL_ENV "/Users/zakharovellia/code/vpn-site/python/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
